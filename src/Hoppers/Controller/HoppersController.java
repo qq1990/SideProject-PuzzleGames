b1b393f0cdc9e07173e95d46ac8abe58278cfc52
Quan Quy <qq1990@g.rit.edu>
2021-06-03 21:09:06 -0400

    Hoppers completed

--- src/Hoppers/Controller/HoppersController.java
+++ src/Hoppers/Controller/HoppersController.java
@@ -1,44 +1,34 @@
 package Hoppers.Controller;
 
-import Hoppers.GUI.HoppersGUI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import BFS.Observer;
 import Hoppers.Model.HoppersClientData;
 import Hoppers.Model.HoppersModel;
-import javafx.stage.FileChooser;
-import java.io.File;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class HoppersController implements Observer<HoppersModel, HoppersClientData> {
-    private final static String RESOURCES_DIR = "Resources/";
+    private final static String RESOURCES_DIR = "Images/";
     private HoppersModel model;
     private ArrayList<Button> buttonsList = new ArrayList<>();
-    private boolean rebuildGui = false;
-    private Stage stage;
 
     private final Image redFrog = new Image(getClass().getResourceAsStream(RESOURCES_DIR+"red_frog.png"));
     private final Image greenFrog = new Image(getClass().getResourceAsStream(RESOURCES_DIR+"green_frog.png"));
@@ -52,55 +42,28 @@ public class HoppersController implements Observer<HoppersModel, HoppersClientDa
     private Label topLabel;
 
     @FXML
-    private Button resetButton;
-
-//    @FXML
-//    private Button loadButton;
-
-    @FXML
-    private Button hintButton;
-
-
-
+    private ComboBox<String> puzzleChoices;
 
 
     public void initialize(){
+        hoppersBorderPane.setStyle("-fx-background-color: #0091ea;");
+        ObservableList<String> list = FXCollections.observableArrayList("Puzzle 1", "Puzzle 2", "Puzzle 3",
+                "Puzzle 4", "Puzzle 5", "Puzzle 6", "Puzzle 7");
+        puzzleChoices.setItems(list);
+        String filename = "data/hoppers/hoppers-7.txt";
         try {
-//            String filename = getParameters().getRaw().get(0);
-            String filename = "data/hoppers/hoppers-4.txt";
             this.model = new HoppersModel(filename);
             model.addObserver(this);
         }
         catch (IOException e){
-//            System.out.println("Can't open file: " + getParameters().getRaw().get(0));
+            System.out.println("Can't open file");
         }
-//        HoppersGUI hoppersGUI = new HoppersGUI();
-//        BorderPane borderPane = hoppersGUI.getBorderPane();
         //clear the buttonsList for the new stage and make the new stage
         this.buttonsList.clear();
-//        this.stage = stage;
-
-        //create file chooser
-        final FileChooser fileChooser = new FileChooser();
 
         //create layouts and buttons
-        BorderPane borderPane = new BorderPane();
-//        this.topLabel = new Label("Loaded: " + getParameters().getRaw().get(0));
         this.topLabel.setText("Loaded puzzle");
         GridPane gridPane = new GridPane();
-
-//        HBox bottomHbox = new HBox();
-//        Button loadButton = new Button("Load");
-//        Button resetButton = new Button("Reset");
-//        Button hintButton = new Button("Hint");
-//        BorderPane bottomBorderPane = new BorderPane();
-
-//        //bottom buttons
-//        bottomHbox.getChildren().addAll(loadButton,resetButton,hintButton);
-//        bottomHbox.setAlignment(Pos.CENTER);
-//
-//        //bottom scene
-//        bottomBorderPane.setCenter(bottomHbox);
 
         //center
         for (int row=0; row<this.model.getCurrentConfig().getNumRows(); ++row) {
@@ -130,22 +93,6 @@ public class HoppersController implements Observer<HoppersModel, HoppersClientDa
         //set main border
         gridPane.setAlignment(Pos.CENTER);
         hoppersBorderPane.setCenter(gridPane);
-
-//        //buttons get pressed
-//        loadButton.setOnAction(event -> {
-//            File file = fileChooser.showOpenDialog(stage);
-//            if (file != null) {
-//                this.rebuildGui = true;
-//                this.model.load(file.toString());
-//            }
-//        });
-
-//        resetButton.setOnAction(event -> {
-//            model.reset();
-//        });
-//        hintButton.setOnAction(event -> {
-//            model.hint();
-//        });
     }
 
     @FXML
@@ -168,16 +115,29 @@ public class HoppersController implements Observer<HoppersModel, HoppersClientDa
     }
 
 
+    @FXML
+    private void handlePuzzleChoices(){
+        switch (puzzleChoices.getValue()) {
+            case "Puzzle 1" -> model.load("data/hoppers/hoppers-1.txt");
+            case "Puzzle 2" -> model.load("data/hoppers/hoppers-2.txt");
+            case "Puzzle 3" -> model.load("data/hoppers/hoppers-3.txt");
+            case "Puzzle 4" -> model.load("data/hoppers/hoppers-4.txt");
+            case "Puzzle 5" -> model.load("data/hoppers/hoppers-5.txt");
+            case "Puzzle 6" -> model.load("data/hoppers/hoppers-6.txt");
+            case "Puzzle 7" -> model.load("data/hoppers/hoppers-7.txt");
+        }
+    }
+
+
 
     @Override
     public void update(HoppersModel hoppersModel, HoppersClientData hoppersClientData) {
-        if (this.rebuildGui){
-//            start(this.stage);
-            this.rebuildGui = false;
-        }
+        GridPane gridPane = new GridPane();
         this.topLabel.setText(hoppersClientData.toString());
         for (int row=0; row<this.model.getCurrentConfig().getNumRows(); ++row) {
             for (int col=0; col<this.model.getCurrentConfig().getNumColumns(); ++col) {
+                int tempRow = row;
+                int tempCol = col;
                 if(this.model.getCurrentConfig().getBoard()[row][col]=='.'){
                     buttonsList.get(row*this.model.getCurrentConfig().getNumColumns()+col).setGraphic(new ImageView(lilyPad));
                 }
@@ -190,9 +150,13 @@ public class HoppersController implements Observer<HoppersModel, HoppersClientDa
                 else if(this.model.getCurrentConfig().getBoard()[row][col]=='*'){
                     buttonsList.get(row*this.model.getCurrentConfig().getNumColumns()+col).setGraphic(new ImageView(water));
                 }
+                gridPane.add(buttonsList.get(row*this.model.getCurrentConfig().getNumColumns()+col), col, row);
+                this.buttonsList.add(buttonsList.get(row*this.model.getCurrentConfig().getNumColumns()+col));
+                buttonsList.get(row*this.model.getCurrentConfig().getNumColumns()+col).setOnAction(event -> model.select(tempRow, tempCol));
             }
         }
+        //set main border
+        gridPane.setAlignment(Pos.CENTER);
+        hoppersBorderPane.setCenter(gridPane);
     }
-
-
 }
